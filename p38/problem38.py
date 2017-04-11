034d86793760c2b7172b7dd94e9feefbd9e28b49
Stanislav Mach <email@stanl3y.cz>
2017-04-11 12:00:33 +0200

    add p38: count and say

---
+++ p38/problem38.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+
+        # sequence stored as a list of integers
+        nums_list = self.next_step([1], n-1)
+        return "".join([ str(x) for x in nums_list ])
+
+
+    def next_step(self, nums, n):
+      if n == 0:
+        return nums
+
+      new_nums = []
+      slow, fast = 0,0
+
+      while slow < len(nums):
+        while fast < len(nums) and nums[fast] == nums[slow]:
+          fast += 1
+
+        new_nums.extend([fast - slow, nums[slow]])
+        slow = fast
+
+      return self.next_step(new_nums, n-1)
+
+
+
+
+        
+
+import unittest
+
+class ProblemTest(unittest.TestCase):
+  def test(self):
+    self.assertEqual("1", Solution().countAndSay(1))
+    self.assertEqual("111221", Solution().countAndSay(5))
+    self.assertEqual("13112221", Solution().countAndSay(7))
+
+
+if __name__ == '__main__':
+  unittest.main()
