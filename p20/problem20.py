38d8cdde9d1c883573ec649785a6947833fc3a8b
Stanislav Mach <email@stanl3y.cz>
2017-04-13 23:33:49 +0200

    add p20: valid parenthesis

---
+++ p20/problem20.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def isValid(self, string):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+        corresp_bracket = { "(": ")", "[": "]", "{": "}"}
+        open_brackets = corresp_bracket.keys()
+
+        for bracket in string:
+            if bracket in open_brackets:
+                stack.append(bracket)
+            else:
+                if not (stack and bracket == corresp_bracket[stack.pop()]):
+                    return False
+
+        return True if not stack else False
+
+      
+
+import unittest
+
+class ProblemTest(unittest.TestCase):
+  def test(self):
+    self.assertEqual(True, Solution().isValid(""))
+    self.assertEqual(True, Solution().isValid("([{}])"))
+    self.assertEqual(True, Solution().isValid("({})[]"))
+
+    # not all brackets closed
+    self.assertEqual(False, Solution().isValid("({}"))
+
+    # closing the wrong bracket
+    self.assertEqual(False, Solution().isValid("(}"))
+
+    # closing an unopened bracket
+    self.assertEqual(False, Solution().isValid("]"))
+
+if __name__ == '__main__':
+  unittest.main()
