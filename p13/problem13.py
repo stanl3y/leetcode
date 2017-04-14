a97a7c9c76bfd6ad9edd770ffc6c7d45c0f48d61
Stanislav Mach <email@stanl3y.cz>
2017-04-14 23:05:44 +0200

    add p13: roman to integer

---
+++ p13/problem13.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        value = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 }
+        summands = []
+
+        for i, part in enumerate(s):
+            val = value[part]
+
+            subtract = (i+1) < len(s) and value[part] < value[s[i+1]]
+            sign = -1 if subtract else 1
+
+            summands.append(sign * val)
+
+        return sum(summands)
+
+
+
+import unittest
+
+class ProblemTest(unittest.TestCase):
+  def test(self):
+        cases = {0: ""}
+        cases.update({1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M" })
+        cases.update({2: "II", 9: "IX", 49: "XLIX", 94: "XCIV"})
+
+        for integer, roman in cases.items():
+          self.assertEqual(integer, Solution().romanToInt(roman))
+
+if __name__ == '__main__':
+  unittest.main()
