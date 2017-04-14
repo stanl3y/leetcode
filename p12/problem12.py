4a32d2959f0a9b9e18e9b69c4f46eeeb272e1f34
Stanislav Mach <email@stanl3y.cz>
2017-04-14 21:48:18 +0200

    add p12: integer to roman numeral

---
+++ p12/problem12.py
@@ -0,0 +1,62 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        digits = {
+                0: {1: "I", 5: "V"},
+                1: {1: "X", 5: "L"},
+                2: {1: "C", 5: "D"},
+                3: {1: "M"}
+        }
+
+        def level(x):
+            return digits[curr_level][x]
+
+        def translate(dig):
+            if dig == 9:
+                return level(1) + digits[curr_level+1][1]
+            elif dig > 4:
+                return level(5) + translate(dig - 5)
+            elif dig == 4:
+                return level(1) + level(5)
+            else:
+                return dig * level(1)
+
+
+
+        curr_level = 0
+        rev_result = []
+
+        num_list = [int(x) for x in list(str(num))]
+        
+        for dig in reversed(num_list):
+            rev_result.append(translate(dig))
+            curr_level += 1
+
+        return "".join(reversed(rev_result))
+
+
+
+
+
+        
+
+
+
+import unittest
+
+class ProblemTest(unittest.TestCase):
+  def test(self):
+
+    
+    cases = {}
+    cases.update({1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M" })
+    cases.update({2: "II", 9: "IX", 49: "XLIX", 94: "XCIV"})
+
+    for integer, roman in cases.items():
+      self.assertEqual(roman, Solution().intToRoman(integer))
+
+if __name__ == '__main__':
+  unittest.main()
