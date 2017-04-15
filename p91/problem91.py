d5f88ef398909e858a2fd032922d8b9490b43000
Stanislav Mach <email@stanl3y.cz>
2017-04-15 20:52:59 +0200

    add p91: decode ways

---
+++ p91/problem91.py
@@ -0,0 +1,128 @@
+from operator import mul 
+from functools import reduce
+import re
+
+class Solution(object):
+    def numDecodings(self, string):
+        if not string: return 0
+
+        symbols = self.extract_symbols_efficient(string)
+
+        # alternatively EITHER
+        # segments = self.extract_segments(string)
+        # symbols = self.segments_to_symbols(segments)
+
+        # alternatively OR  
+        # segments = self.extract_segments_regex(string)
+        # symbols = self.segments_to_symbols(segments)
+
+        num_decodings = self.calc_decodings(symbols)
+        return num_decodings
+
+    def calc_decodings(self, symbols):
+        if not symbols: return 0
+
+        # process symbols
+        symbols = "".join(symbols)
+        segm_lens = [len(x) for x in symbols.split("x")]
+
+        # calculate the required no. of Fibonacci terms
+        fib = [1,1]
+        for _ in range(max(segm_lens)):
+            fib.append(fib[-1] + fib[-2])
+
+        # if (F_1, F_2, F_3 ...) = (1, 1, 2)
+        # then n dashes ~ F_(n+2) options ~ fib[n+1]
+        segm_ways = [fib[n+1] for n in segm_lens]
+
+        return reduce(mul, segm_ways)
+
+    def extract_segments_regex(self, string):
+        # zeros always need a digit in front (1 or 2)
+        if re.search('^0', string) or re.search('00', string): return []
+
+        zeros_occur = re.findall('[1-9]0', string)
+        if zeros_occur and not set(zeros_occur) < set(['10', '20']): return []
+
+        # split into independent segments
+        return re.split('[1-9]0', string)
+
+
+
+    def extract_segments(self, string):
+        segments, symbols = [], []
+        slow = 0
+
+        # note that zeros split string into independent segments
+        # (zero must have a digit in front, cannot have 0 or 01)
+        for i in range(len(string)):
+            if string[i] == "0":
+                if i-1 >= 0 and int(string[i-1:i+1]) in [10,20]:
+                    segments.append(string[slow:i-1])
+                    slow = i+1
+                else: return [] 
+            if i == len(string) - 1:
+                segments.append(string[slow : len(string)])
+
+        return segments
+
+
+
+    def segments_to_symbols(self, segments):
+        symbols = []
+
+        # translate segments as "-" if two consecutive digits can be joined and "x" otherwise
+        for segment in segments:
+            for i in range(1, len(segment)):
+                symbols.append( "-" if 11 <= int(segment[i-1:i+1]) <= 26 else "x")
+            symbols.append("x") # segments are independent
+        
+        return symbols
+
+
+
+
+    def extract_symbols_efficient(self, string):
+        symbols = []
+        i = len(string) - 1
+
+        # going backwards, check all pairs
+        while i > 0:
+            current_pair = int(string[i-1:i+1])
+
+            if not string[i] == "0":
+                symbols.append("-" if 11 <= current_pair <= 26 else "x")
+                i -= 1
+            else:
+                if current_pair in [10,20]:
+                    symbols.append("xx")
+                    i -= 2
+                else:
+                    return 0
+
+        # and check also the first element
+        if string[0] == "0": return []
+        else: symbols.append("x")
+
+        return symbols
+
+
+
+import unittest
+
+class ProblemTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(0, Solution().numDecodings(""))
+        self.assertEqual(5, Solution().numDecodings("1111"))
+        self.assertEqual(15, Solution().numDecodings("1119111"))
+        self.assertEqual(5, Solution().numDecodings("11234"))
+        
+        self.assertEqual(0, Solution().numDecodings("0"))
+        self.assertEqual(0, Solution().numDecodings("11130"))
+        self.assertEqual(2, Solution().numDecodings("11201"))
+        self.assertEqual(1, Solution().numDecodings("1"))
+
+
+
+if __name__ == '__main__':
+  unittest.main()
