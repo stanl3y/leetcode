a723815fa4af77f53a049d03071ab6279792f74f
Stanislav Mach <email@stanl3y.cz>
2017-04-30 10:14:44 +0100

    add p35: search for insert position

---
+++ p35/problem35.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        lo, hi = 0, len(nums)
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid
+
+        return lo 
+
+
+
+import unittest
+
+class ProblemTest(unittest.TestCase):
+  def test(self):
+    self.assertEqual(2, Solution().searchInsert([1,2,3,4], 3)) # find middle
+    self.assertEqual(2, Solution().searchInsert([1,2,4,5], 3)) # insert middle
+
+    self.assertEqual(0, Solution().searchInsert([1,2,3,4], 0)) # insert left edge
+    self.assertEqual(4, Solution().searchInsert([1,2,3,4], 5)) # insert right edge
+
+
+
+    
+
+if __name__ == '__main__':
+  unittest.main()
