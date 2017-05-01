9ccfe4d91430d9a058836f505905a92739a6b011
Stanislav Mach <email@stanl3y.cz>
2017-05-01 16:50:25 +0100

    add p99: jump game (read model solution)

---
+++ p55/problem55.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        need_to_jump = 0
+
+        for i in range( len(nums)-2, -1, -1):
+          need_to_jump += 1
+
+          if nums[i] >= need_to_jump:
+            need_to_jump = 0
+
+        return True if not need_to_jump else False
+        
+
+import unittest
+
+class ProblemTest(unittest.TestCase):
+  def test(self):
+    #self.assertEqual(0, Solution().insert_function())
+    self.assertEqual(True, Solution().canJump([2,3,1,1,4]))
+    self.assertEqual(False, Solution().canJump([3,2,1,0,4]))
+
+if __name__ == '__main__':
+  unittest.main()
