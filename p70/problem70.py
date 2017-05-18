e69176602031481771b718fd947b25016571513b
Stanislav Mach <email@stanl3y.cz>
2017-05-18 19:41:57 +0100

    add p70: climbing stairs

---
+++ p70/problem70.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 0: return 0
+
+        # idea: given that we can take either one or two steps
+        # the number of ways to climb follows the Fibonacci sequence 
+
+        fib = [1,1]
+        for _ in range(n-1):
+            fib.append( fib[-2] + fib[-1] )
+
+        return fib[n]
+
+
+import unittest
+
+class ProblemTest(unittest.TestCase):
+    def test(self):
+        cases = {1:1, 2:2, 3:3, 5:8}
+
+        for when, expect in cases.items():
+            self.assertEqual(expect, Solution().climbStairs(when))
+
+if __name__ == '__main__':
+    unittest.main()
